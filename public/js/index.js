62ea97da7c2ad0644827cb7dc7e229063368d1c1
Tomás Mudano <mudano.tomas@gmail.com>
2019-01-31 16:48:05 -0300

    Added form to submit messages and UI to receive them

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,19 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
   console.log('New message', message);
+  let li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#messageForm').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function() {
+
+  });
 });
